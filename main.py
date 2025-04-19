f207ff83e28f3dddf9ab42049dfa4e8f12c8989a
! ⋆.˚ Inf3cted - ⋆ ♡ <152812000+suicid4lcupcake@users.noreply.github.com>
2025-04-19 05:42:12 +0100

    Update main.py
    Some Slightly Changement

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
-from AesEverywhere import aes256
 from src import *
 from colorama import Fore
 from encrypt import *
 from decrypt import *
+from cryptography.fernet import Fernet
+import os 
 print(Fore.CYAN + icon)
 
-option = input(Fore.RED + '[1]-Encrypt\n[2]-Decrypt\n[99]-Exit\n==>')
+option = input(Fore.YELLOW + '[1]-Encrypt\n[2]-Decrypt\n[99]-Exit\n==>')
 if option == '1':
-    option = input(Fore.RED + '[1]-Single File\n[2]-Mass Files\n==>')
+    option = input(Fore.BLUE + '[1]-Single File\n[2]-Mass Files\n==>')
     if option == '1':
+        key = Fernet.generate_key()
         filename = input('Filename :')
-        password = input('Password :')
-        if filename and password:
-            encrypt(filename , password)
+        print(Fore.YELLOW + f'[+] Generating new key ==> secret.key')
+        with open('secret.key' , 'wb') as file:
+            file.write(key)
+        encrypt(filename , key)
     elif option == '2':
         folder_path = input('Folder_Path :')
         password = input('Password :')
         if folder_path and password:
             mass_encrypt(folder_path , password)
 elif option == '2':
-    option = input(Fore.RED + '[1]-Single File\n[2]-Mass Files\n==>')
+    option = input(Fore.YELLOW + '[1]-Single File\n[2]-Mass Files\n==>')
     if option == '1':
         filename = input('Filename :')
-        password = input('Password :')
-        if filename and password:
-            decrypt(filename , password)
+        key_file = input('Key File :')
+        if not  os.path.exists(key_file):
+            print(Fore.RED + 'Key File Do Not Exists')
+        else:
+            with open(key_file , 'rb') as file:
+                key = file.read()
+            decrypt(filename , key)
     elif option == '2':
         folder_path = input('Folder_Path :')
         password = input('Password :')
         if folder_path and password:
             mass_decrypt(folder_path , password)
+print(Fore.WHITE + '')
